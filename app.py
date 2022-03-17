271e16aad1c648d80d6ef71d1bc407c55c9303d6
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-03-17 21:28:22 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return 'About'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
