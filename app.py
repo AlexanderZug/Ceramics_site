0e98571e24df9734514e9ddab7e0beaea0fccd72
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-03-19 15:17:22 +0300

    add environment variables

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+import os
+
 from flask import Flask, render_template, url_for
+from person_data import TELEGRAM, WHATS_UP, VK_PAGE
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', telegram=TELEGRAM, whats_up=WHATS_UP, vk_page=VK_PAGE)
 
 
 @app.route('/about')
---
+++ person_data.py
@@ -0,0 +1,5 @@
+import os
+
+TELEGRAM = os.environ.get('TELEGRAM')
+VK_PAGE = os.environ.get('VK_PAGE')
+WHATS_UP = os.environ.get('WHATS_UP')
