f03dc7c4242ac105ecafcb423b46c0f882239645
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-03-22 23:16:32 +0300

    fix carousel

--- static/js/index_page.js
+++ static/js/index_page.js
@@ -1,12 +1,10 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -14,20 +12,15 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
 
 slideIndex = 0;
+showSlides();
 showSlides();
 
 function showSlides() {
