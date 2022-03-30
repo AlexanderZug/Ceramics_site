baf8956e6b17afcf54ca7aa5d3994f70c1e808d1
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-03-30 23:18:22 +0300

    add contact form and new icons to base.html

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ from person_data import TELEGRAM, WHATS_UP, VK_PAGE
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def base():
     return render_template('index.html', telegram=TELEGRAM, whats_up=WHATS_UP, vk_page=VK_PAGE)
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html', telegram=TELEGRAM, whats_up=WHATS_UP, vk_page=VK_PAGE)
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html', telegram=TELEGRAM, whats_up=WHATS_UP, vk_page=VK_PAGE)
+# @app.route('/about')
+# def about():
+#     return render_template('about.html', telegram=TELEGRAM, whats_up=WHATS_UP, vk_page=VK_PAGE)
+#
+#
+# @app.route('/contact')
+# def contact():
+#     return render_template('contact.html', telegram=TELEGRAM, whats_up=WHATS_UP, vk_page=VK_PAGE)
 
 
 if __name__ == '__main__':
