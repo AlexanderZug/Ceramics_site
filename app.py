6e49cbd09a629bd3b23e020a2ee01d8243e4ec7f
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-03-31 20:01:43 +0300

    add sop section to base.html

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, url_for
 from person_data import TELEGRAM, WHATS_UP, VK_PAGE
 
-
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def base():
     return render_template('index.html', telegram=TELEGRAM, whats_up=WHATS_UP, vk_page=VK_PAGE)
-
 
 
 if __name__ == '__main__':
