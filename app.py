2993c5e6e2073a5400dcb6e1ec9a0084c95dd6c1
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-04-07 23:02:25 +0300

    add postgres integration

--- app.py
+++ app.py
@@ -1,27 +1,41 @@
 from flask import Flask, request, flash, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 import re
 
-from person_data import TELEGRAM, WHATS_UP, VK_PAGE
+from person_data import TELEGRAM, WHATS_UP, VK_PAGE, DB_CONFIG
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'eiwbi8839183y84iuqh39731hx'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONFIG
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
 
 regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
+
+class Client(db.Model):
+    __tablename__ = 'clients'
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(40))
+    email=db.Column(db.String(40))
+    message = db.Column(db.String(150))
 
 
 @app.route('/', methods=['POST', 'GET'])
 def base():
     if request.method == 'POST':
         rec = request.form
-        name = rec.get('name')
-        email = rec.get('email')
-        message = rec.get('message')
-        if len(name) < 2:
-            flash('Your name must be at least 3 characters in length')
-        elif not re.fullmatch(regex, email):
-            flash("Your e-mail isn't correct.")
+        # name = rec.get('name')
+        # email = rec.get('email')
+        # message = rec.get('message')
+        if len(rec.get('name')) < 2:
+            flash('Your name must be at least 3 characters in length', category='danger')
+        elif not re.fullmatch(regex, rec.get('email')):
+            flash("Your e-mail isn't correct.", category='danger')
         else:
-            flash('Your message sent.')
+            flash('Your message sent.', category='success')
     return render_template('base.html', telegram=TELEGRAM, whats_up=WHATS_UP, vk_page=VK_PAGE)
 
 
--- person_data.py
+++ person_data.py
@@ -3,3 +3,4 @@ import os
 TELEGRAM = os.environ.get('TELEGRAM')
 VK_PAGE = os.environ.get('VK_PAGE')
 WHATS_UP = os.environ.get('WHATS_UP')
+DB_CONFIG = os.environ.get('postgresql://postgres:1234@127.0.0.1/postgres')
