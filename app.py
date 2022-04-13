c4a8abefc82ef86d680c84a2c81550c6839771ef
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-04-13 21:53:41 +0300

    add Toastr

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask, request, flash, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
 import re
 import models
 import mail_sender
-
+from flask import Flask, request, flash, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 from config import Config
 from person_data import TELEGRAM, WHATS_UP, VK_PAGE
+from flask_toastr import Toastr
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 mail = Mail(app)
+toastr = Toastr(app)
 regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
 
@@ -20,11 +22,11 @@ def base():
     if request.method == 'POST':
         rec = request.form
         if len(rec.get('name')) < 2:
-            flash('Your name must be at least 3 characters in length', category='danger')
+            flash('Your name must be at least 3 characters in length', 'error')
         elif not re.fullmatch(regex, rec.get('email')):
-            flash("Your e-mail isn't correct.", category='danger')
+            flash("Your e-mail isn't correct.", 'error')
         else:
-            flash('Your message sent.', category='success')
+            flash('Your message sent.', 'success')
             client = models.Client(rec.get('name'), rec.get('email'), rec.get('message'))
             db.session.add(client)
             db.session.commit()
