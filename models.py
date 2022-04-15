4b727d68749697981c693a16e3433fe4c7d5f154
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-04-15 21:15:45 +0300

    refactoring html

--- models.py
+++ models.py
@@ -5,10 +5,10 @@ class Client(db.Model):
     """Class for sending data to the database."""
 
     __tablename__ = 'clients'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40))
-    email = db.Column(db.String(40))
-    message = db.Column(db.String(150))
+    id: int = db.Column(db.Integer, primary_key=True)
+    name: str = db.Column(db.String(40))
+    email: str = db.Column(db.String(40))
+    message: str = db.Column(db.String(150))
 
     def __init__(self, name: str, email: str, message: str) -> None:
         self.name = name
